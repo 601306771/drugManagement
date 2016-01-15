40a691b8b8756bb6bb9a1e85fb79add23ef5f974
unknown <LINI8@CS-VEC020-W7.corp.oocl.com>
2016-01-15 17:25:20 +0800

    2016-15 公司
    已完成：
    1.用户管理
    2.供应商管理
    3.药品管理
         2）药品分类

--- src/main/java/silver/api/newDrugRequest/dao/NewDrugRequestDao.java
+++
@@ -1,9 +0,0 @@
-package silver.api.newDrugRequest.dao;
-
-import java.util.List;
-
-import silver.api.newDrugRequest.entity.NewDrugRequestEntity;
-
-public interface NewDrugRequestDao {
-	List<NewDrugRequestEntity> selectByStatus(String status);
-}
--- src/main/java/silver/api/user/mapper/userMapper.java
+++
@@ -1,9 +0,0 @@
-package silver.api.user.mapper;
-
-import silver.api.user.entity.user;
-
-public interface userMapper {
-
-    user query();
-
-}
